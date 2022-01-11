92c06510d5e93410ccc2245d97285dd9523960a1
Pablo Romero <90pabloromero@gmail.com>
2022-01-11 17:22:12 -0300

    fixing cors

--- next.config.js
+++ next.config.js
@@ -14,10 +14,10 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "/api/(.*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "true" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
