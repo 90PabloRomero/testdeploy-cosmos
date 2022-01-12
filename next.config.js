8990cacc8f31110d2fc9667a57cdef8fd88c71b1
Pablo Romero <90pabloromero@gmail.com>
2022-01-12 01:48:59 -0300

    fixing cors

--- next.config.js
+++ next.config.js
@@ -28,6 +28,6 @@ module.exports = {
     apiUrl:
       process.env.NODE_ENV === "development"
         ? "http://localhost:3000/api" // development api
-        : "http://localhost:3000/api", // production api
+        : "http://76.76.21.21:3000/api", // production api
   },
 };
