4eed6d67b1aac84fa2f739d7cc6c2f9b34f1f6a9
Pablo Romero <90pabloromero@gmail.com>
2022-01-12 00:57:28 -0300

    fixing cors

--- pages/api/pruebas/now.js
+++ pages/api/pruebas/now.js
@@ -1,6 +1,6 @@
 export default function now(req, res) {
   const date = new Date();
-  const format = date.toGMTSTring();
+  const format = date.toGMTString();
 
   res.setHeader("cache-control", "s-maxage=10, stale-while-revalidate");
 
