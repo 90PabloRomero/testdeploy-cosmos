90261d0d5b444bda0c2f4d8956431e864dbfdf4b
Pablo Romero <90pabloromero@gmail.com>
2022-01-13 10:02:57 -0300

    fix imports

--- pages/broker/panel.jsx
+++ pages/broker/panel.jsx
@@ -1,12 +1,12 @@
-import { leadService } from "../services";
+import { leadService } from "services";
 
 import { useEffect, useState } from "react";
 import { Link } from "components";
 import CalendarArea from "components/Calendar";
 import Image from "next/image";
-import addIcon from "add.png";
-import editIcon from "edit.png";
-import sendIcon from "icon3.png";
+import addIcon from "public/add.png";
+import editIcon from "public/edit.png";
+import sendIcon from "public/icon3.png";
 
 const Home = ({ onClick, onClickUser }) => {
   const [leads, setLeads] = useState(null);
