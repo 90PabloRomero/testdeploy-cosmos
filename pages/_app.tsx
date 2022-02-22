0ba5e7092a491088a684932115323a5c85a78b30
Fariza0727 <farizayandiyeva@gmail.com>
2022-02-23 01:39:01 +0600

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,71 +1,13 @@
 import Head from "next/head";
-import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { userService } from "../services";
-import { Nav, Alert, SideBar, Modal } from "../components";
 import GlobalStyles from "../styles/globalStyles";
-import { ModalUser } from "../components/demo/modalUser";
-import { ModalCotizacion } from "../components/demo/modalCotizacion";
-import { ModalCargarCotizacion } from "../components/demo/modalCargarCotizacion";
 import { createWrapper } from 'next-redux-wrapper';
 import configureStore from '../redux/store';
 // export default App;
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-  // const [user, setUser] = useState(null);
-  // const [authorized, setAuthorized] = useState(false);
-  // const [modalVisible, setModalVisible] = useState(false);
-  // const [modalUserVisible, setModalUserVisible] = useState(false);
-  // const [modalCotizacionVisible, setModalCotizacionVisible] = useState(false);
-  // const [sideBarVisible, setSideBarOpen] = useState(false);
-  // const [modalCCVisible, setModalCCVisible] = useState(false);
-  // useEffect(() => {
-  //   document.body.className = modalCotizacionVisible ? "modal-mode" : "";
-  //   document.body.className = modalCCVisible ? "modal-mode" : "";
-  //   document.body.className = modalUserVisible ? "modal-mode" : "";
-  //   document.body.className = modalVisible ? "modal-mode" : "";
-  // });
-  // useEffect(() => {
-  //   // on initial load - run auth check
-  //   authCheck(router.asPath);
-
-  //   // on route change start - hide page content by setting authorized to false
-  //   const hideContent = () => setAuthorized(false);
-  //   router.events.on("routeChangeStart", hideContent);
-
-  //   // on route change complete - run auth check
-  //   router.events.on("routeChangeComplete", authCheck);
-
-  //   // unsubscribe from events in useEffect return function
-  //   return () => {
-  //     router.events.off("routeChangeStart", hideContent);
-  //     router.events.off("routeChangeComplete", authCheck);
-  //   };
-
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
-
-  // function authCheck(url: string) {
-  //   // redirect to login page if accessing a private page and not logged in
-  //   setUser(userService.userValue);
-  //   const publicPaths = [
-  //     "/broker/login",
-  //     "/aseguradoras/login",
-  //     "/clientes/login",
-  //   ];
-  //   const path = url.split("?")[0];
-  //   if (!userService.userValue && !publicPaths.includes(path)) {
-  //     setAuthorized(false);
-  //     router.push({
-  //       pathname: "/broker/login",
-  //       query: { returnUrl: router.asPath },
-  //     });
-  //   } else {
-  //     setAuthorized(true);
-  //   }
-  // }
-
+  
   return (
     <>
       <GlobalStyles />
@@ -75,55 +17,7 @@ function MyApp({ Component, pageProps }) {
       <>
         <Component {...pageProps} />
       </>
-      {/* <Modal
-        modalVisible={modalVisible}
-        onOutsideClick={() => setModalVisible(false)}
-      />
-      <ModalUser
-        modalUserVisible={modalUserVisible}
-        onOutsideClick={() => {
-          setModalUserVisible(false);
-        }}
-      />
-      <ModalCotizacion
-        modalCotizacionVisible={modalCotizacionVisible}
-        onOutsideClick={() => {
-          setModalCotizacionVisible(false);
-        }}
-      />
-      <ModalCargarCotizacion
-        modalCCVisible={modalCCVisible}
-        onOutsideClick={() => {
-          setModalCCVisible(false);
-        }}
-      />
-
-      <div className={`app-container`}>
-        {user && <Nav />}
-        <div className="tooltip-wrapper">
-          <SideBar
-            sideBarVisible={sideBarVisible}
-            onOutsideClick={() => setSideBarOpen(false)}
-          />
-          <div
-            className={`layout ${sideBarVisible ? " layout-modal-mode" : ""} `}
-          >
-            <Alert />
-
-            {authorized && (
-              <>
-                <Component
-                  {...pageProps}
-                  onClick={setModalVisible}
-                  onClickUser={setModalUserVisible}
-                  onClickCotizacion={() => setModalCotizacionVisible(true)}
-                  onClickCargarCotizacion={() => setModalCCVisible(true)}
-                />
-              </>
-            )}
-          </div>
-        </div> */}
-      {/* </div> */}
+      
     </>
   );
 }
